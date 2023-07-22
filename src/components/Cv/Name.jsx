b9382d351f257ee90ab1e01c5caf4cf5e98771e0
Becherair-Abdellah <abdeallahdz2004@gmail.com>
2023-07-22 14:38:51 +0100

    complteted summary component

--- src/components/Cv/Name.jsx
+++ src/components/Cv/Name.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Name() {
   return (
-    <div className=' border-[3px] border-l-0 border-t-0 pr-8 pb-5 border-green-600 relative -top-10'>
+    <div className=' border-[3px] border-l-0 border-t-0 pr-8 pb-5 border-green-600 relative mb-8'>
       <h1 className='text-4xl font-medium mb-2'>Abdellah Becherair</h1>
       <span className='text-xl text-green-600 font-medium '>Front end Developer</span>
     </div>
--- src/components/Cv/Summary.jsx
+++ src/components/Cv/Summary.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import { FaCircle } from "react-icons/fa"
 
 function Summary() {
   return (
-    <div>
-     
+   <div className="relative">
+   <h1 className='text-2xl text-gray-500 mb-3 flex items-center gap-2 relative' ><FaCircle className='w-3 h-3 text-green-700' size={10}/> Summary</h1>
+    <div className="text-[17px] psdo">
+     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam incidunt rerum dicta distinctio? Velit ex sed, et vero doloremque ea.
     </div>
+   </div>
   )
 }
 
