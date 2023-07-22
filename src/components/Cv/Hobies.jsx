fa59197b796353ee69b99fc10f18cd4612fb9c1b
Becherair-Abdellah <abdeallahdz2004@gmail.com>
2023-07-22 10:57:15 +0100

    finish Name component

--- src/components/Cv/Hobies.jsx
+++ src/components/Cv/Hobies.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Hobies() {
   return (
     <div>
-      Hobies
+   
     </div>
   )
 }
--- src/components/Cv/Name.jsx
+++ src/components/Cv/Name.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 function Name() {
   return (
-    <div>
-      Name
+    <div className=' border-4 border-l-0 border-t-0 pr-8 pb-5 border-green-600'>
+      <h1 className='text-4xl font-medium mb-2'>Abdellah Becherair</h1>
+      <span className='text-xl text-green-600 font-medium '>Front end Developer</span>
     </div>
   )
 }
