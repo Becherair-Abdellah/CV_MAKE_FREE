5768fed9587a8901a1ad90f4efbd5400bd8076c7
Becherair-Abdellah <abdeallahdz2004@gmail.com>
2023-07-22 10:44:07 +0100

    editing the places of components usnig tailwind and now start work with components

--- src/components/Cv/Cv.jsx
+++ src/components/Cv/Cv.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
-
+import Name from './Name'
+import Summary from './Summary'
+import Contact from './Contact'
+import Projects from './Projects'
+import Skills from './Skills'
+import Languages from './Languages'
+import Education from './Education'
+import Hobies from './Hobies'
 function Cv() {
   return (
-    <div className='w-[794px] h-[1123px] bg-gray-500 mx-auto'>
-      abdellah
+    <div className='w-[794px] h-[1123px] bg-gray-100 mx-auto p-4'>
+      <div className='children' >
+        <div className="top flex gap-3 items-center">
+          <div className="twocompo">
+            <Name/>
+            <Summary/>
+          </div>
+          <Contact/>
+        </div>
+        <div className="bottom flex items-center">
+          <div className='projects' >
+            <Projects/>
+          </div>
+          <div className="allcompon">
+            <Skills/>
+            <Languages/>
+            <Education/>
+            <Hobies/>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
